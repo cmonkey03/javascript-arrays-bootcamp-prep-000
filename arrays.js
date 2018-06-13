7f498bb12b239b883a8b0e8dc712bc55b8addc58
russellpbrown@gmail.com <russellpbrown@gmail.com>
2018-06-13 04:27:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  var slice = array.slice(1)
   return array
 }
 
